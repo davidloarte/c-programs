a9fd6591cd0f60982062614aeebbf75165c9fbdd
davidloarte <davidloarte@yahoo.es>
2017-12-15 11:01:04 +0100

    corregido

--- 04_QBoss/projet.cpp
+++ 04_QBoss/projet.cpp
@@ -16,9 +16,8 @@ int main(){
 
     //salida de datos
     printf(" %.2lf, %.2lf, %.2lf => \t", vertice[X], vertice[Y], vertice[Z]);
-    printf(" %.2lf, %.2lf \n", vertice[X] / vertice[Z],
-                               vertice[Y] / vertice[Z]);
-
+    printf("( %.2lf, %.2lf )\n", vertice[X] / vertice[Z],
+                               -vertice[Y] / vertice[Z]);
 
 
     return EXIT_SUCCESS;
